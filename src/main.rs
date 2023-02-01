890a587fe8fbe15ec86771b6a0ce254d1351c0ea
Simon Schneemelcher <schneemelchercom@gmail.com>
2023-02-01 20:18:50 +0100

    try implementing calling executables

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use std::process::Command;
+
 macro_rules! shell {
     (printf $e:tt$(;)*) => {{
         print!("{}", $e);
     }};
+    ($cmd:ident $($arg:tt) *) => {
+        let mut cmd = Command::new(stringify! {$cmd});
+        // cmd.args(vec! {$($arg) *});
+        match cmd.spawn() {
+            Ok(mut child) => match child.stdout.take() {
+                None => {}
+                Some(output) => println! {"{:#?}", output},
+            },
+            Err(_) => println! {"{}: command not found", stringify! {$cmd}},
+        }
+    };
 }
 
 fn main() {
     shell! {
         printf "Hello World!\n";
+    }
+
+    shell! {
+        ls
+    }
+
+    shell! {
+        foo
     }
 }
